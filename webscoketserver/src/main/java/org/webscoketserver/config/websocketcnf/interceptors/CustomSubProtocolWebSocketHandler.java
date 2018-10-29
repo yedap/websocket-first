e3da520931a5584eaf6101287f711b08b4b9112d
yedap <1226625421@qq.com>
2018-10-29 13:38:45 +0800

    修改前端页面

--- webscoketserver/src/main/java/org/webscoketserver/config/websocketcnf/interceptors/CustomSubProtocolWebSocketHandler.java
+++ webscoketserver/src/main/java/org/webscoketserver/config/websocketcnf/interceptors/CustomSubProtocolWebSocketHandler.java
@@ -1,7 +1,10 @@
 package org.webscoketserver.config.websocketcnf.interceptors;
 
 import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.messaging.MessageChannel;
 import org.springframework.messaging.SubscribableChannel;
@@ -24,11 +27,25 @@ public class CustomSubProtocolWebSocketHandler extends SubProtocolWebSocketHandl
 	@Override
 	public void afterConnectionEstablished(WebSocketSession session) throws Exception {
 		List<String> os = session.getHandshakeHeaders().get("cookie");
-		String s  = os.get(0);
-		System.out.println(s);
-		System.out.println(session.getAttributes().toString());
-		webSocketSessionContainer.registere(session.getId(), session);
-		// TODO Auto-generated method stub
+		for (String string : os) {
+			  //JSESSIONID=DA60D23BC0A5928F38BA8E5F11FFC21C
+		      String pattern = "(JSESSIONID=)(.{32})";
+		      // 创建 Pattern 对象
+		      Pattern r = Pattern.compile(pattern);
+		      // 现在创建 matcher 对象
+		      Matcher m = r.matcher(string);
+		      if (m.find( )) {
+		         System.out.println("Found value: " + m.group(0));
+		         System.out.println("Found value: " + m.group(1)); 
+		         System.out.println("Found value: " + m.group(2)); 
+		         String id =m.group(2);
+		         if(StringUtils.isNotBlank(id)){
+		        	 webSocketSessionContainer.registere(id, session);
+		         }
+		      } else {
+		         System.out.println("NO MATCH");
+		      }
+		}
 		super.afterConnectionEstablished(session);
 	}
 	
